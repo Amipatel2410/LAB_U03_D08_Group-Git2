bf0e1b0619c1a930cf8f0fb8ac0828c503246a4a
Ari Brenner <ari.brenner@generalassemb.ly>
2017-08-15 09:34:12 -0400

    add gender

--- person.js
+++ person.js
@@ -1,10 +1,12 @@
 class Person {
-  constructor(name, age) {
+  constructor(name, age, gender) {
     this.name = name;
     this.age = age;
+    this.gender = gender;
   }
 
   sayHi() {
     console.log(`Hi, I am ${this.name}`)
+    console.log(`My gender is ${this.gender}`)
   }
 }
